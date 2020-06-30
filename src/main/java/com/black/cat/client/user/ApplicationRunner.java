d34c9f56c8d55a65c76ff2aa37aa7b42cff9a3fe
Kavichezliyan Rajendran <kavichezliyan.rajendran@adp.com>
2020-06-30 11:33:49 -0400

    initial data load on mongo db

--- src/main/java/com/black/cat/client/user/ApplicationRunner.java
+++ src/main/java/com/black/cat/client/user/ApplicationRunner.java
@@ -3,9 +3,8 @@
  */
 package com.black.cat.client.user;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationListener;
-import org.springframework.stereotype.Component;
 
 import com.black.cat.client.user.model.User;
 import com.black.cat.client.user.service.UserService;
@@ -20,23 +19,6 @@ import lombok.extern.slf4j.Slf4j;
 //@Component
 @RequiredArgsConstructor
 @Slf4j
-public class ApplicationRunner implements ApplicationListener<ApplicationEvent> {
-	
-	private final UserService userService;
-
-	@Override
-	public void onApplicationEvent(ApplicationEvent event) {
-		User user = User.builder().emailId("k@g.com")
-				.username("username").password("password")
-				.mobileNo(1234567890l).role("U").build();
-		userService.saveUser(user);
-		log.info("created user successfully- {}",user.getUsername());
-		User user1 = User.builder().emailId("k1@g.com")
-				.username("username1").password("password1")
-				.mobileNo(1234567891l).role("A").build();
-		userService.saveUser(user1);
-		log.info("created user successfully- {}",user1.getUsername());
-
-	}
+public class ApplicationRunner {
 
 }
--- src/main/java/com/black/cat/client/user/UserserviceApplication.java
+++ src/main/java/com/black/cat/client/user/UserserviceApplication.java
@@ -1,14 +1,39 @@
 package com.black.cat.client.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import com.black.cat.client.user.model.User;
+import com.black.cat.client.user.service.UserService;
+
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
 @EnableEurekaClient
 @RefreshScope
-public class UserserviceApplication {
+@Slf4j
+public class UserserviceApplication implements CommandLineRunner { 
+	
+	@Autowired
+	private UserService userService;
+
+	@Override
+	public void run(String... args) throws Exception {
+		User user = User.builder().emailId("k@g.com")
+				.username("username").password("password")
+				.mobileNo(1234567890l).role("U").build();
+		userService.saveUser(user);
+		log.info("created user successfully- {}",user.getUsername());
+		User user1 = User.builder().emailId("k1@g.com")
+				.username("username1").password("password1")
+				.mobileNo(1234567891l).role("A").build();
+		userService.saveUser(user1);
+		log.info("created user successfully- {}",user1.getUsername());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
